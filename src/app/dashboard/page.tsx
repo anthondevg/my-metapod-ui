754418e9b99d14305b2dfa607489f6414ba68e5e
anthondevg <robwert7991@gmail.com>
2023-06-20 21:15:35 -0400

    boilerplate next,tailwind, twin.micro, babel

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ipsum reiciendis quidem, ratione quo odit in delectus numquam, voluptate sunt repudiandae iure inventore labore facilis accusantium commodi recusandae? Tenetur, recusandae.
+    </div>
+  )
+}
+
+export default Dashboard;
